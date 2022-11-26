9eb146619e53429eee573536e7fee93ca1c93841
Srimathi822 <112163602+Srimathi822@users.noreply.github.com>
2022-11-26 22:39:16 +0530

    Update app.py

--- Assignments/Team member 2/Assignment 2/app.py
+++ Assignments/Team member 2/Assignment 2/app.py
@@ -1,21 +1,51 @@
-from flask import Flask,render_template
-app=Flask(__name__)
+print(account)
+ if account:
+ session['loggedin'] = True
+ session['id'] = account['USERNAME']
+ userid = account["USERNAME"]
+ session['username'] = account["USERNAME"]
+ msg = 'Logged in successfully!'
+ return redirect(url_for('welcome', username=username))
+ else: msg = "Incorrect Username/Password"
+ return render_template('login.html', msg = msg)
+ @app.route('/reg2',methods=["GET","POST"])
+ def registration():
+ msg = " "
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
+ if request.method=="POST":
+ username = request.form['uname']
+ email = request.form['email']
+ password = request.form['pwd']
+ rollno = request.form['rollno']
+ sql="SELECT * FROM USERS WHERE USERNAME=?"
+ stmt=ibm_db.prepare(conn,sql)
+ ibm_db.bind_param(stmt,1,username)
+ ibm_db.execute(stmt)
+ account = ibm_db.fetch_assoc(stmt)
+ print(account)
+ if account:
+ msg = "Account already exists!"
+ elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+ msg = "Invalid Email Address."
+ elif not re.match(r'[A-Za-z0-9]+', username):
+ msg = "Username must contain only alphabets and numbers."
+ else:
+ insert_sql = "INSERT INTO USERS VALUES(?,?,?,?)"
+ prep_stmt = ibm_db.prepare(conn,insert_sql)
+ ibm_db.bind_param(prep_stmt,1,email)
+ ibm_db.bind_param(prep_stmt,2,username)
+ ibm_db.bind_param(prep_stmt,3,rollno)
+ ibm_db.bind_param(prep_stmt,4,password)
+ ibm_db.execute(prep_stmt)
+ msg = "You have successfully registered."
+ return render_template('login.html', msg=msg)
 
-@app.route('/signin')
-def signin():
-    return render_template('signin.html')
+ elif request.method == 'POST': msg="Please fill out the form."
+ return render_template('reg.html',msg=msg)
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+ @app.route('/welcome/<username>')
+ def welcome(username):
+ return "Welcome %s!" %username
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-if __name__=='__main__':
-    app.run(host='0.0.0.0', port=8081, debug=True)
+ if __name__=="__main__":
+ app.run(host='0.0.0.0')
